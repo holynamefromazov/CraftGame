67a3fed4b2f33d9809e546ba51ae32eb30c313e3
Дорохин Святослав <svyatoslav.dorokhin@playrix.com>
2026-05-02 17:43:17 +0500

    Added controlling camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,55 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour, InputControll.IPlayerActions
+public class PlayerController : MonoBehaviour, InputControl.IPlayerActions
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
-    private InputControll inputControll;
+    [SerializeField] private float cameraSensitivity = 1f;
+    [SerializeField] private Camera playerCamera;
+
+    private InputControl InputControl;
     private Rigidbody rb;
     private Vector2 moveInput;
     private Vector3 moveDirection;
+    private Vector2 lookDelta;
+    private Vector2 cameraRotation;
     private bool isGrounded;
 
     private void Awake()
     {
-        inputControll = new InputControll();
+        InputControl = new InputControl();
         rb = GetComponent<Rigidbody>();
 
         // Назначаем этот скрипт как обработчик
-        inputControll.Player.SetCallbacks(this);
+        InputControl.Player.SetCallbacks(this);
     }
 
     private void OnEnable()
     {
-        inputControll.Player.Enable();
+        InputControl.Player.Enable();
     }
 
     private void OnDisable()
     {
-        inputControll.Player.Disable();
+        InputControl.Player.Disable();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraControl();
     }
 
     // Реализация интерфейса
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        DetectDevice(context);
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -48,27 +59,42 @@ public class PlayerController : MonoBehaviour, InputControll.IPlayerActions
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+        DetectDevice(context);
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
         if (context.performed)
             Debug.Log("Attack");
+        DetectDevice(context);
     }
 
     public void OnInteract(InputAction.CallbackContext context)
     {
         if (context.performed)
             Debug.Log("Interact");
+        DetectDevice(context);
     }
 
+    public void OnCameraСontrol(InputAction.CallbackContext context)
+    {
+        lookDelta = context.ReadValue<Vector2>();
+        DetectDevice(context);
+    }
+    private void CameraControl()
+    {
+        cameraRotation.x += lookDelta.x * cameraSensitivity;
+        cameraRotation.y += lookDelta.y * cameraSensitivity;
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -90f, 90f);
+        playerCamera.transform.localRotation = Quaternion.Euler(-cameraRotation.y, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, cameraRotation.x, 0);
+    }
     private void Move()
     {
         moveDirection.Set(moveInput.x, 0, moveInput.y);
         if (moveDirection.magnitude > 1.01f)
         {
             moveDirection.Normalize();
-            Debug.Log($"Move Input: {moveInput} and Move Direction: {moveDirection}");
         }
 
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
@@ -81,4 +107,12 @@ public class PlayerController : MonoBehaviour, InputControll.IPlayerActions
             isGrounded = true;
         }
     }
+
+    private void DetectDevice(InputAction.CallbackContext context)
+    {
+        var device = context.control.device;
+        bool isGamepad = device is Gamepad;
+        Debug.Log("Device used:" + (isGamepad ? "Gamepad" : "Keyboard/Mouse"));
+    }
+
 }
